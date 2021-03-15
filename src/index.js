93354ad5cfea3cb77055b5e1f481583a98eb59bd
regin99 <regintmor@icloud.com>
2021-03-16 02:25:45 +0300

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    if (n < 0) {
+        n = -n;
+    }
+    n += "";
+    return parseInt(n.split("").reverse().join(""));
+};
